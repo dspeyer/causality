0e60ff9b23f12ebe6068906a1168591d84914811
Daniel Speyer <dspeyer@gmail.com>
2017-04-21 02:38:58 -0400

    three modes of direction test

--- src/test_direction.py
+++ src/test_direction.py
@@ -6,7 +6,7 @@ from scipy.stats import chi2_contingency
 from scipy.stats import chi2
 from scipy.stats import chisquare
 from math import log
-from sys import stdout
+from sys import stdout, argv, exit
 from random import random
 from simulate_causal_net import simulate
 from collections import defaultdict
@@ -28,49 +28,99 @@ n = 58
 p_nod2 = 0.39
 p_cd_given_nod2=[0.48, 0.91]
 
+mode=argv[1][1:]
+if mode not in ['platonic', 'crohns', 'multi']:
+    print "Bad Mode"
+    exit(1)
+runs=int(argv[2])
+
+
 i=0
-for _ in range(10000):
+tries=0
+while i < runs:
+    tries+=1
+    if tries % (runs/10)==0:
+        print 'try #%d / run #%d ' % (tries, i)
     net=struct()
-    net.a = p_nod2
-    #    p_bact = (random()*.4)+.4
-    p_bact = random()
-    net.b_given_a=[p_bact, p_bact]
+
+    if mode=='platonic':
+        net.a = random()
+    else:
+        net.a = p_nod2
+
+    if mode=='platonic':
+        p_bact = random()
+    else:
+        p_bact = (random()*.4)+.4
+    if mode!='multi':
+        net.b_given_a=[p_bact, p_bact]
+    else:
+        net.b_given_a=[p_bact, p_bact + random()*0.2 - 0.1]
+
     net.c_given_ab=[[0,0],[0,0]]
-    while min(net.c_given_ab[0][1],net.c_given_ab[1][1])<=0 or max(net.c_given_ab[0][1],net.c_given_ab[1][1])>=1:
-        net.c_given_ab[0][0] = random()
-        net.c_given_ab[1][0] = (net.c_given_ab[0][0] + (random()*.4+.4)) % 1
-        for g in [0,1]:
-            net.c_given_ab[g][1] = (p_cd_given_nod2[g]-net.c_given_ab[g][0]*(1-p_bact))/p_bact
-    #print net
+    if mode=='platonic':
+        for a in range(2):
+            for b in range(2):
+                net.c_given_ab[a][b]=random()
+    else:
+        while min(net.c_given_ab[0][1],net.c_given_ab[1][1])<=0 or max(net.c_given_ab[0][1],net.c_given_ab[1][1])>=1:
+            net.c_given_ab[0][0] = random()
+            net.c_given_ab[1][0] = (net.c_given_ab[0][0] + (random()*.4+.4)) % 1
+            for g in [0,1]:
+                net.c_given_ab[g][1] = (p_cd_given_nod2[g]-net.c_given_ab[g][0]*(1-p_bact))/p_bact
+
     data = simulate(net, n)
-    cnt = count(zip(*data))
-#    if any(cnt, lambda(x):x<=5):
-#        continue
-    #print cnt
+
+    if mode!='platonic':
+        cnt = count(zip(*data))
+        if any(cnt, lambda(x):x==0):
+            continue
+        if chi2_contingency(count(zip(data[0],data[2])))[1]>.01:
+            continue
+        if mode=='multi' and chi2_contingency(count(zip(data[0],data[1])))[1]<.1:
+            continue
+
     dir=direction(data[0], data[2], data[1], n, p_nod2, p_cd_given_nod2)
-    #print dir.bayes_fwd_rev
     record(dir, True)
     i += 1
 
-print '%d fwd' % i
-
-#print "---"
+print '%d fwd (took %d tries)' % (i, tries)
 
 i=0
-for _ in range(10000):
+tries=0
+while i < runs:
     net=struct()
-    net.a = p_nod2
-    net.b_given_a = p_cd_given_nod2
-    net.c_given_ab=[random(), random()]
-#    net.c_given_ab=[random(), 0]
-#    net.c_given_ab[1] = (net.c_given_ab[0] + (random()*.4+.4)) % 1
-    net.c_given_ab = [net.c_given_ab]*2
-    #print net
+    tries += 1
+
+    if mode=='platonic':
+        net.a = random()
+    else:
+        net.a = p_nod2
+
+    if mode=='platonic':
+        net.b_given_a = [random(), random()]
+    else:
+        net.b_given_a = p_cd_given_nod2
+
+    if mode!='multi':
+        net.c_given_ab=[random(), random()]
+        net.c_given_ab = [net.c_given_ab]*2
+    else:
+        p_bact_given_cd = [ random() * 0.4 + 0.1 ]
+        p_bact_given_cd.append( p_bact_given_cd[0] + random()*0.4  )
+        net.c_given_ab=[[ p_bact_given_cd[0], p_bact_given_cd[1] ],
+                        [ p_bact_given_cd[0] + random()*0.2 - 0.1, p_bact_given_cd[1] + random()*0.2 - 0.1 ]]
+
     data = simulate(net, n)
-    cnt = count(zip(*data))
-#    print cnt
-#    if any(cnt, lambda(x):x<=5):
-#        continue
+    if mode!='platonic':
+        cnt = count(zip(*data))
+        if any(cnt, lambda(x):x==0):
+            continue
+        if chi2_contingency(count(zip(data[0],data[1])))[1]>.001:
+            continue
+        if mode=='multi' and chi2_contingency(count(zip(data[0],data[2])))[1]<.01:
+            continue
+
     try:
         dir=direction(data[0], data[1], data[2], n, p_nod2, p_cd_given_nod2)
         #print dir.bayes_fwd_rev
@@ -79,7 +129,7 @@ for _ in range(10000):
     except ValueError:
         print 'ValueError'
 
-print '%d rev' % i
+print '%d rev (took %d tries)' % (i, tries)
 
 for i in range(cats+1):
     print '%f: %f (%d)' % (i/float(cats), tot[i] and float(hit[i])/tot[i], tot[i])
